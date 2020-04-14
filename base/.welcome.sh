3d0545a1483ff031653e35021f6a99735437f55c
omicsgene <huang2002003@qq.com>
2020-04-14 09:46:40 +0800

    add welcome

---
+++ base/.welcome.sh
@@ -0,0 +1,8 @@
+echo "欢迎使用组学大讲堂提供的docker镜像：www.omicsclass.com"
+echo ""
+echo "Linux新手建议学习："
+echo "--> https://www.omicsclass.com/article/702"
+echo ""
+echo "关于实验室生物信息分析平台搭建与docker使用详情："
+echo "--> https://www.omicsclass.com/article/1181"
+
