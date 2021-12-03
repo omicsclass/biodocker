bff3167f086fd6d7d737a6c7295b0eb582e74de7
omicsgene <huang2002003@qq.com>
2021-12-03 10:40:53 +0800

    添加了GWAS分析软件和本地构建docker

--- rstudio-server/w.sh
+++ rstudio-server/w.sh
@@ -1,5 +1,4 @@
-rm -rf R-v4.0.3
-cp -r /share/work/biosoft/R/R-v4.0.3/ .
-docker build -t omicsclass/r-server:latest .
-docker push omicsclass/r-server:latest
+#rm -rf R-v4.0.3
+#cp -r /share/work/biosoft/R/R-v4.0.3/ .
+docker build -t omicsclass/r-server:latest . && docker push omicsclass/r-server:latest
 
