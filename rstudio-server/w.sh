0ada75ed97c93ed497f336ec26b2ee13c5258a19
omicsgene <huang2002003@qq.com>
2022-04-20 13:38:35 +0800

    add python

--- rstudio-server/w.sh
+++ rstudio-server/w.sh
@@ -1,4 +1,6 @@
 rm -rf R-v4.0.3
+rm -rf Python-v3.8.12
 cp -r /share/work/biosoft/R/R-v4.0.3/ .
+cp -r /share/work/biosoft/python/Python-v3.8.12 .
 docker build -t omicsclass/r-server:latest . && docker push omicsclass/r-server:latest
 
