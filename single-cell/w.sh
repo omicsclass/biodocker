f10a3b06a91f1b7c022a0ee598c39a510fd36f73
omicsgene <huang2002003@qq.com>
2022-05-20 17:15:25 +0800

    小优化

--- single-cell/w.sh
+++ single-cell/w.sh
@@ -3,3 +3,6 @@
 #cp -r /share/work/biosoft/R/R-v4.0.3/ .
 #cp -r /share/work/biosoft/python/Python-v3.8.12 .
 docker build -t omicsclass/single-cell:latest .
+docker tag omicsclass/single-cell:latest omicsclass/single-cell:v2.0
+docker push omicsclass/single-cell:latest
+docker push omicsclass/single-cell:v2.0
