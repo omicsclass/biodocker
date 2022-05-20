a5a7ba13886ea3293bbb19ec3e06779637572657
omicsgene <huang2002003@qq.com>
2022-05-20 17:09:48 +0800

    单细胞2.0上线

--- single-cell/w.sh
+++ single-cell/w.sh
@@ -1,1 +1,5 @@
+#rm -rf R-v4.0.3
+#rm -rf Python-v3.8.12
+#cp -r /share/work/biosoft/R/R-v4.0.3/ .
+#cp -r /share/work/biosoft/python/Python-v3.8.12 .
 docker build -t omicsclass/single-cell:latest .
